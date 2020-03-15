6971b1efd5f4a0940b069aeed1af605168f1bda3
fullStack-Blake <koreanincanada92@gmail.com>
2020-03-15 12:32:37 -0400

    Movie application base

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//(<App> is JSX) JSX only use one component at a time
+// JSX is specific for react only
+// Component - just a function returns html
+ReactDOM.render(<App />, document.getElementById("root"));
