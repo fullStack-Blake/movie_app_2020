fd660882dc907d787def054ec8258fc28a17069c
fullStack-Blake <koreanincanada92@gmail.com>
2020-03-15 16:36:57 -0400

    css styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -27,13 +28,13 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
+      <section className="container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader_text">Loading...</span>
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
           </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map(movie => (
               <Movie
                 key={movie.id}
@@ -42,6 +43,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
           </div>
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from "./App";
 
 //(<App> is JSX) JSX only use one component at a time
 // JSX is specific for react only
+
 // Component - just a function returns html
 ReactDOM.render(<App />, document.getElementById("root"));
